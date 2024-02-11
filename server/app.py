e4890674a59471c06f1d3b6917b7cc6e139560d2
Colton Woodruff <coltwood93@gmail.com>
2024-02-11 12:23:04 -0800

    adjusted sleeve recommendation temperature and removed unused exception variable

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
+from os import environ
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-from os import environ
 import requests
 
 app = Flask(__name__)
@@ -12,6 +12,8 @@ SECRET_KEY = environ.get('API_KEY')
 
 @app.route("/get_recommendation", methods=["GET"])
 def get_recommendation():
+    """Requests temperature through OpenWeatherMap API, 
+    then recommends sleeve length based on return data"""
     location = request.args.get("location")  # Retrieve location from query parameters
     api_url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={SECRET_KEY}"
 
@@ -20,9 +22,9 @@ def get_recommendation():
         data = response.json()
         temperature_kelvin = data["main"]["temp"]
         temperature_fahrenheit = (temperature_kelvin - 273.15)*9/5 + 32
-        recommendation = "short sleeves" if temperature_fahrenheit > 70 else "long sleeves"
+        recommendation = "short sleeves" if temperature_fahrenheit > 69 else "long sleeves"
         return jsonify({'temperature': temperature_fahrenheit, 'recommendation': recommendation})
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException:
         return jsonify({'error': 'Invalid request format'}), 400
 
 if __name__ == "__main__":
