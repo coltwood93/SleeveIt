4834b16019840f311ee19b4e62ea3c3e9f2e673e
Colton Woodruff <coltwood93@gmail.com>
2024-02-27 22:42:11 -0800

    added clear search button

--- sleeve-it/src/components/SearchBar.vue
+++ sleeve-it/src/components/SearchBar.vue
@@ -9,11 +9,21 @@
   const getRecommendation = async () => {
     emit('get-recommendation', location.value);
   };
+
+  const clearSearch = () => {
+    const confirmClear = window.confirm('Are you sure you want to clear the search?');
+    if (confirmClear) {
+      location.value = ''; // Clear the search bar
+    }
+  };
+
 </script>
 
 <template>
   <div>
     <input type="text" placeholder="Enter city name" v-model="location" @keyup.enter="getRecommendation">
     <button @click="getRecommendation">Get Recommendation</button>
+    <br>
+    <button @click="clearSearch">Clear</button>
   </div>
 </template>
