be30b6e215777e80cfe26bca7ff61b553aeff72b
Colton Woodruff <coltwood93@gmail.com>
2024-03-04 19:31:49 -0800

    added partners microservice

---
+++ microservice/micro_server.py
@@ -0,0 +1,30 @@
+import os
+import requests
+import json
+import random
+import time
+
+from flask import Flask, json
+
+api = Flask(__name__)
+
+@api.route('/cities', methods=['GET'])
+def get_cities():
+    with open("./cities.json",'r') as file:
+        cities = json.load(file)
+        keys = list(cities.keys())
+        rand_key = int(random.random() * 1000 % 51)        
+        state = (keys[rand_key])
+        #print(state)
+        city = (cities[state])
+        rand_city = int(random.random() * 1000 % len(city))
+        location = city[rand_city]
+        #print(location)
+        response = {"city":location, "state":state}
+        #print(response)
+        #response_json = json.dumps(response)
+        #print(response_json)
+    return json.dumps(response)
+
+if __name__ == '__main__':
+    api.run(debug=False, port=5001)
